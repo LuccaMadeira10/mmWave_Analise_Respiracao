32e4e09ab81a751fe3c22a3ac3439c5727e10985
Henrique Issao Kato <114307623+HenriqueIK@users.noreply.github.com>
2025-08-29 18:01:25 -0300

    Add files via upload

---
+++ Monitoramentosinais_copy_20250729191322.ino
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include "Seeed_Arduino_mmWave.h"
+
+// Set up serial communication depending on the board type
+#ifdef ESP32
+#  include <HardwareSerial.h>
+HardwareSerial mmWaveSerial(0);
+#else
+#  define mmWaveSerial Serial1
+#endif
+
+SEEED_MR60BHA2 mmWave;
+
+void setup() {
+Serial.begin(9600);
+mmWave.begin(&mmWaveSerial);
+}
+
+void loop() {
+if (mmWave.update(100)) {
+ float total_phase, breath_phase, heart_phase;
+ if (mmWave.getHeartBreathPhases(total_phase, breath_phase, heart_phase)) {
+   Serial.print(breath_phase);
+   Serial.print(",");
+   Serial.println(20);
+ }
+}
+}
